3024796ecf69a5a2a6cff0344b5d197c8d645f9a
Léo LE BRAS <leo.lebrasf@gmail.com>
2015-11-27 23:44:30 +0100

    Cleaner

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,1 @@
-console.log(`It works \o/ !`);
-document.querySelector('.Default_content').innerHTML = 'It works \o/ !';
+document.querySelector('.Default_content').innerHTML = `It works &#92;o/ !`;
