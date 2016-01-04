6c2e54ea9266611b49280494164265cdbfde9122
Léo LE BRAS <leo.lebrasf@gmail.com>
2016-01-04 22:22:28 +0100

    [webpack] Resolve js/vendors and js/utils

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,36 @@
 import config from './config.js';
+import path from 'path';
+
 let entry = {};
 config.javascript.entry.map(item => {
     entry = { ...entry, [item]: `${config.dir.srcDir}${config.dir.jsDir}${item}` };
 });
 
-module.exports = {
+export default {
     devtool: 'source-map',
     entry: entry,
     output: {
         path: config.dir.distDir + config.dir.jsDir,
-        filename: '[name]',
+        filename: '[name]'
+    },
+    resolve: {
+        extensions: ['', '.js'],
+        modulesDirectories: [
+            'node_modules',
+            'src/js/',
+            'src/js/vendors',
+            'src/js/utils'
+        ]
     },
     watch: true,
     module: {
         loaders: [{
             loader: 'babel-loader',
-            query: config.javascript.babel
+            query: config.javascript.babel,
+            exclude: [
+                path.resolve(__dirname, 'node_modules/'),
+                path.resolve(__dirname, 'src/js/vendors/')
+            ]
         }]
     }
 };
